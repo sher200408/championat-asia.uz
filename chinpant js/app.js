728f3216ba735af8686cbdadf631582017965e7a
shermuhammad shertayev backend <113122559+sher200408@users.noreply.github.com>
2023-09-27 01:00:48 +0500

    Add files via upload

---
+++ chinpant js/app.js
@@ -0,0 +1,19 @@
+// let theFox = document.querySelector("#dog1") 
+// let theDog = document.querySelector("#dog2") 
+// let theCat = document.querySelector("#dog3") 
+
+function myTheimg () {
+    document.querySelector("#dog1").style.display="block"
+    document.querySelector("#dog2").style.display="none"
+    document.querySelector("#dog3").style.display="none"
+}
+function myTheimggol () {
+    document.querySelector("#dog1").style.display="none"
+    document.querySelector("#dog2").style.display="block"
+    document.querySelector("#dog3").style.display="none"
+}
+function myTheimgol () {
+    document.querySelector("#dog1").style.display="none"
+    document.querySelector("#dog2").style.display="none"
+    document.querySelector("#dog3").style.display="block"
+}
